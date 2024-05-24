47d5721b4e007f19dbb2a9d7e93d34844070933b
yukun <yukun0507@outlook.jp>
2024-05-24 16:20:36 +0900

    Update: supported \

--- src/readline.c
+++ src/readline.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 03:22:27 by yushsato          #+#    #+#             */
-/*   Updated: 2024/04/14 22:23:38 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/05/24 16:12:14 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,23 @@
 */
 char	*ms_readline(void)
 {
-	char	*input;
 	char	*prompt;
+	char	*input;
+	char	*extra;
+	char	*tmp;
 
 	prompt = ms_prompt();
 	input = readline(prompt);
 	free(prompt);
+	while (input && input[ft_strlen(input) - 1] == '\\')
+	{
+		tmp = input;
+		extra = readline("> ");
+		input[ft_strlen(input) - 1] = '\0';
+		input = ft_strjoin(input, extra);
+		free(tmp);
+		free(extra);
+	}
 	if (input && input[0] != '\0')
 		add_history(input);
 	return (input);
