c07f10e95ce26dfafbd7dbdc6425efc4faf82902
yukun <yukun0507@outlook.jp>
2024-06-10 16:45:43 +0900

    Update: sanitize

--- src/parser/sanitize.c
+++ src/parser/sanitize.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 15:42:35 by yushsato          #+#    #+#             */
-/*   Updated: 2024/06/10 16:40:02 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/06/10 16:44:22 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,10 @@
 t_token	*sanitize_token(t_token *head)
 {
 	t_token	*courser;
-	t_token	*tmp;
 
 	courser = head;
 	while (courser)
 	{
-		if (courser->token == NULL || courser->token[0] == '\0')
-		{
-			tmp = courser->next;
-			if (courser->prev)
-				courser->prev->next = courser->next;
-			else
-				head = courser->next;
-			if (courser->next)
-				courser->next->prev = courser->prev;
-			free(courser->token);
-			free(courser);
-			courser = tmp;
-			continue ;
-		}
 		courser->len = ft_strlen(courser->token);
 		courser = courser->next;
 	}
