a11545d3e48af2a667fc13b5c1a747f1ee6b27b2
yukun <yukun0507@outlook.jp>
2024-06-16 16:41:33 +0900

    Update: mv debug function

--- src/execute/run.c
+++ src/execute/run.c
@@ -6,42 +6,13 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 04:17:16 by yushsato          #+#    #+#             */
-/*   Updated: 2024/06/16 16:23:20 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/06/16 16:30:00 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-t_node	*execute_ready(t_token *cursor)
-{
-	t_node	*head;
-	t_node	*node;
-
-	node = ft_calloc(1, sizeof(t_node));
-	head = node;
-	while (cursor)
-	{
-		if (LXR_PIPE <= cursor->type)
-		{
-			node->conjection_type = cursor->type;
-			if (cursor->next)
-			{
-				node->next = NODE().new(node);
-				node->next->prev = node;
-				node = node->next;
-			}
-			cursor = cursor->next;
-		}
-		else if (LXR_INPUT <= cursor->type)
-			cursor = NODE().add_redir(node, cursor);
-		else
-			cursor = NODE().add_args(node, cursor);
-	}
-	return (head);
-}
-
-/**
- * # WIP
+/** WIP
  * node_print - For debugging, print the node structure
  */
 void	node_print(t_node *node)
@@ -77,11 +48,39 @@ void	node_print(t_node *node)
 	);
 }
 
+t_node	*execute_ready(t_token *cursor)
+{
+	t_node	*head;
+	t_node	*node;
+
+	node = ft_calloc(1, sizeof(t_node));
+	head = node;
+	while (cursor)
+	{
+		if (LXR_PIPE <= cursor->type)
+		{
+			node->conjection_type = cursor->type;
+			if (cursor->next)
+			{
+				node->next = NODE().new(node);
+				node->next->prev = node;
+				node = node->next;
+			}
+			cursor = cursor->next;
+		}
+		else if (LXR_INPUT <= cursor->type)
+			cursor = NODE().add_redir(node, cursor);
+		else
+			cursor = NODE().add_args(node, cursor);
+	}
+	return (head);
+}
+
 int	execute_run(t_token *cursor, char **envp)
 {
 	t_node	*node;
-	// int		lpipe[2];
-	// int		rpipe[2];
+	int		lpipe[2];
+	int		rpipe[2];
 	
 	node = execute_ready(cursor);
 	while (node)
