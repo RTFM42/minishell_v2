b1efaf1c85c2de60d0567bf213573a4b2432f7d6
yush <yus-sato@kalyte.ro>
2024-06-19 16:33:44 +0900

    Update: norminette

--- src/signal_cache.c
+++ src/signal_cache.c
@@ -6,21 +6,21 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 00:39:16 by yushsato          #+#    #+#             */
-/*   Updated: 2024/06/19 15:58:16 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/06/19 16:25:46 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 static int	*sig_cache(void)
 {
 	static int	sig = 0;
-	
+
 	return (&sig);
 }
 
 void	sig_cache_set(int signo)
 {
 	int	*cache;
-	
+
 	cache = sig_cache();
 	*cache = signo;
 }
@@ -28,7 +28,7 @@ void	sig_cache_set(int signo)
 int	sig_cache_get(void)
 {
 	int	*cache;
-	
+
 	cache = sig_cache();
 	return (*cache);
 }
