5d88b0e2b3e38c2b36212ddc0c51b4bfc9596dc8
yush <yus-sato@kalyte.ro>
2024-06-19 17:28:07 +0900

    Fix: the issue that the code couldn't compile due to unnecessary code

--- src/execute/close2.c
+++ src/execute/close2.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:20:39 by yushsato          #+#    #+#             */
-/*   Updated: 2024/06/19 17:22:39 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/06/19 17:27:08 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,7 @@
 
 void	close_pipe(int *pipe)
 {
-	const int	err1 = sf_close(pipe[0]);
-	const int	err2 = sf_close(pipe[1]);
-
-	if (err1 < 0)
-		sf_error_print("close");
-	if (err2 < 0)
-		sf_error_print("close");
+	sf_close(pipe[0]);
+	sf_close(pipe[1]);
 }
 
