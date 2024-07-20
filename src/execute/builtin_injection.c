e0bc07343f3cd5f84c072076f214ca35c9e745d1
yush <yus-sato@kalyte.ro>
2024-07-21 02:43:01 +0900

    Update

--- src/execute/builtin_injection.c
+++ src/execute/builtin_injection.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_injection.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 14:58:28 by yushsato          #+#    #+#             */
-/*   Updated: 2024/07/15 21:29:17 by nsakanou         ###   ########.fr       */
+/*   Updated: 2024/07/21 02:42:16 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-
-int	isbuiltin(const char *fname)
-{
-	if (!ft_memcmp(fname, "echo", 5) || !ft_memcmp(fname, "cd", 3)
-		|| !ft_memcmp(fname, "pwd", 4) || !ft_memcmp(fname, "export", 7)
-		|| !ft_memcmp(fname, "unset", 6) || !ft_memcmp(fname, "env", 4)
-		|| !ft_memcmp(fname, "exit", 5))
-		return (1);
-	return (0);
-}
 
 int	isbuiltin(const char *fname)
 {
