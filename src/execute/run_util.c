b14f54949eac33dea2651b6b9df3adda4c1536cf
yush <yus-sato@kalyte.ro>
2024-07-22 13:27:31 +0900

    Update: segv

--- src/execute/run_util.c
+++ src/execute/run_util.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 02:13:21 by yushsato          #+#    #+#             */
-/*   Updated: 2024/07/21 02:32:10 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/07/22 13:27:22 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,20 @@ void	exec_logic(t_exec *exec, char **envp)
 		ft_memcpy(exec->ifd, exec->ifp, sizeof(int) * 2);
 	if (exec->ofd[1] == 1)
 		ft_memcpy(exec->ofd, exec->ofp, sizeof(int) * 2);
-	if (!exec->fd_stat && !exec->is_pipe && isbuiltin(exec->node->args[0]))
+	if (!exec->fd_stat && !exec->is_pipe && exec->node->args
+		&& isbuiltin(exec->node->args[0]))
 		exec->status = exec_builtin(exec->node->args, envp, exec->ofd);
-	else if (!exec->fd_stat)
+	else if (!exec->fd_stat && exec->node->args)
 	{
 		exec->is_promise = 1;
 		promise_add(async(exec->node->args, envp, exec->ifd, exec->ofd));
 		if (g_signal != 0)
 			exec->status = g_signal;
 		if (exec->heredoc != NULL)
-		{
 			write(exec->ifd[1], exec->heredoc, ft_strlen(exec->heredoc));
-			free(exec->heredoc);
-		}
 	}
+	if (exec->heredoc != NULL)
+		free(exec->heredoc);
 	if (exec->is_pipe)
 		close_pipe(exec->ifp);
 	exec->is_logic = 1;
@@ -51,7 +51,7 @@ void	exec_pipe(t_exec *exec, char **envp)
 		ft_memcpy(exec->ifd, exec->ifp, sizeof(int) * 2);
 	if (exec->ofd[1] == 1)
 		ft_memcpy(exec->ofd, exec->ofp, sizeof(int) * 2);
-	if (!exec->fd_stat)
+	if (!exec->fd_stat && exec->node->args)
 	{
 		exec->is_promise = 1;
 		promise_add(async(exec->node->args, envp, exec->ifd, exec->ofd));
@@ -59,10 +59,9 @@ void	exec_pipe(t_exec *exec, char **envp)
 			exec->status = g_signal;
 	}
 	if (exec->heredoc)
-	{
 		write(exec->ifd[1], exec->heredoc, ft_strlen(exec->heredoc));
+	if (exec->heredoc)
 		free(exec->heredoc);
-	}
 	if (exec->is_pipe)
 		close_pipe(exec->ifp);
 	exec->is_logic = 0;
@@ -75,7 +74,8 @@ void	exec_normal(t_exec *exec, char **envp)
 		ft_memcpy(exec->ifd, exec->ifp, sizeof(int) * 2);
 	if (exec->ofd[1] == 1)
 		ft_memcpy(exec->ofd, exec->ofp, sizeof(int) * 2);
-	if ((exec->is_logic && !exec->status) || !exec->is_logic)
+	if (((exec->is_logic && !exec->status) || !exec->is_logic)
+		&& exec->node->args)
 	{
 		if (!exec->fd_stat && !exec->is_pipe && isbuiltin(exec->node->args[0]))
 			exec->status = exec_builtin(exec->node->args, envp, exec->ofd);
@@ -85,11 +85,12 @@ void	exec_normal(t_exec *exec, char **envp)
 			promise_add(async(exec->node->args, envp, exec->ifd, exec->ofd));
 			if (g_signal != 0)
 				exec->status = g_signal;
-			if (exec->heredoc && write(exec->ifd[1],
-					exec->heredoc, ft_strlen(exec->heredoc)))
-				free(exec->heredoc);
+			if (exec->heredoc)
+				write(exec->ifd[1], exec->heredoc, ft_strlen(exec->heredoc));
 		}
 	}
+	if (exec->heredoc)
+		free(exec->heredoc);
 	if (exec->is_pipe)
 		close_pipe(exec->ifp);
 	exec->is_logic = 0;
