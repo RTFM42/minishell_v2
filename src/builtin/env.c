b1d2e58348f0d4b655aad2f4ba06958378814328
yush <yus-sato@kalyte.ro>
2024-07-25 17:36:23 +0900

    Update: env leaks

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 15:52:03 by yushsato          #+#    #+#             */
-/*   Updated: 2024/07/12 15:29:31 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/07/25 17:36:00 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ int	bt_env(int argc, char *const *argv, char *const *envp)
 		free(tmp);
 	}
 	ft_putstr_fd(res, STDOUT_FILENO);
+	free(res);
 	return (0);
 }
